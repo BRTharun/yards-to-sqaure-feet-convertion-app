92b03ab446efbc4edd9c743fbe40021e80af08d3
B.R.Tharun kumar <brtarunkumar18@gmail.com>
2021-10-12 11:05:05 +0530

    yards to squarefeet conversion app

---
+++ app/src/main/java/com/example/practice1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.practice1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                int yd = Integer.parseInt(s);
+                int sq = yd*9;
+                textView.setText("the value in sq feet ="+sq);
+            }
+        });
+    }
+}
